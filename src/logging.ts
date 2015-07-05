30637def6e65ff90bdb78d534628dc8328916a37
Timo Weiß <info@timo-weiss.com>
2015-07-05 17:20:19 +0200

    add logErr

--- src/logging.ts
+++ src/logging.ts
@@ -17,3 +17,15 @@ export function log(...args:any[]) {
         server.log(['ark-realtime'], args[0]);
     }
 }
+export function logErr(...args:any[]) {
+    if (!server) {
+        console.error('Server not initialized for logging');
+        return;
+    }
+
+    if (args.length > 1) {
+        server.log(['ark-realtime', 'error'], args[0] + ': ' + args.splice(1).join(''));
+    } else {
+        server.log(['ark-realtime', 'error'], args[0]);
+    }
+}
